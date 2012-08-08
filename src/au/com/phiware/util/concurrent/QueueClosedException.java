83abec6475af3ad2b38b7d3c0658feedd47c4ced
Corin Lawson <corin@phiware.com.au>
2012-08-09 01:49:07 +1000

    Fixes whitespace

--- src/au/com/phiware/util/concurrent/QueueClosedException.java
+++ src/au/com/phiware/util/concurrent/QueueClosedException.java
@@ -25,22 +25,22 @@ package au.com.phiware.util.concurrent;
  */
 public class QueueClosedException extends IllegalStateException {
 
-	private static final long serialVersionUID = -8393303638950186454L;
+    private static final long serialVersionUID = -8393303638950186454L;
 
-	public QueueClosedException() {
-		super();
-	}
+    public QueueClosedException() {
+        super();
+    }
 
-	public QueueClosedException(String s) {
-		super(s);
-	}
+    public QueueClosedException(String s) {
+        super(s);
+    }
 
-	public QueueClosedException(Throwable cause) {
-		super(cause);
-	}
+    public QueueClosedException(Throwable cause) {
+        super(cause);
+    }
 
-	public QueueClosedException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public QueueClosedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
